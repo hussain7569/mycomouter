e5f9f7452ed7ea06d58339373ac77faa17035054
hussain7569 <109576956+hussain7569@users.noreply.github.com>
2023-02-24 11:19:58 +0530

    Add files via upload

---
+++ src/test/java/loloj/AppTest.java
@@ -0,0 +1,20 @@
+package loloj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
